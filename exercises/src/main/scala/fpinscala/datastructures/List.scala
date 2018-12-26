92674f029eec6b2abd39b0a66c59dd8a00a91fe0
dantb <dantb@hotmail.co.uk>
2018-12-26 18:07:50 +0000

    Exercises 3.2 - 3.10

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -51,24 +53,76 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil // or throw an exception, or return an Option[List[A]]?
+    case Cons(_, as) => as
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, as) => Cons(h, as)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] = n match {
+    case 0 => l
+    case 1 => tail(l)
+    case _ => drop(tail(l), n-1)
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(a, as) => if (f(a)) dropWhile(as, f) else l
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(a, as) => as match { case Nil => Nil; case _ => Cons(a, init(as)) }
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, total: Int) => total + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+    @tailrec
+    def loop(l: List[A], acc: B): B = l match {
+      case Nil => acc
+      case Cons(a, as) => loop(as, f(acc, a))
+    }
+
+    loop(l, z)
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 
   def main(args: Array[String]): Unit = {
     require(x == 3)
     println("Verified x == 3 in pattern matching exercise (3.1)")
+
+    println("Testing tail:")
+    println("Expected: Nil, Cons(2,Cons(3,Nil))")
+    println("Actual:   %s, %s".format(tail(Nil), tail(List(1, 2, 3))))
+
+    println("Testing drop:")
+    println("Expected: Cons(4,Cons(5,Nil))")
+    println("Actual:   %s".format(drop(List(1, 2, 3, 4, 5), 3)))
+
+    println("Testing dropWhile:")
+    println("Expected: Cons(3,Cons(4,Cons(5,Nil)))")
+    println("Actual:   %s".format(dropWhile(List(1, 2, 3, 4, 5), (i: Int) => i != 3)))
+
+    println("Testing init:")
+    println("Expected: Cons(1,Cons(2,Cons(3,Nil)))")
+    println("Actual:   %s".format(init(List(1, 2, 3, 4))))
+
+    println(s"Testing foldRight behaviour: ${foldRight(List(1, 2, 3), Nil:List[Int])(Cons(_,_))}")
+
+    println("Testing length:")
+    println("Expected: 0 1 5")
+    println("Actual:   %d %d %d".format(length(Nil), length(List(1)), length(List(6, 7, 8, 4, 3))))
+
+    println("Testing foldLeft with sum:")
+    println("Expected: 10")
+    println("Actual:   %d".format(foldLeft(List(1, 2, 3, 4), 0)(_ + _)))
   }
 }
